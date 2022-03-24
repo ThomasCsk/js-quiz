75f10a32b42c3d1cd934b43cd6f6500b884edad8
Thomas Ciszek <tciszek515@gmail.com>
2022-03-24 01:01:09 -0400

    html and css done for the questions section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 var startUpPageEl = document.querySelector("#startup-page");
 var questionsPageEl = document.querySelector("#questions-page");
 var gameOverPageEl = document.querySelector("#game-over-page");
-
-
+var startButtonEl = document.querySelector("#start-button");
+var mainParentEl = document.querySelector("#main-parent");
 
 var startUp = function(){
-questionsPageEl.remove();
-gameOverPageEl.remove();
+  questionsPageEl.remove();
+  gameOverPageEl.remove();
 }
 
+var startQuiz = function(){
+  startUpPageEl.remove();
+  mainParentEl.append(questionsPageEl);
+  /*TimerFunction Start*/
+}
+
+var gameOver = function(){
+  questionsPageEl.remove();
+  mainParentEl.append(gameOverPageEl);
+}
+startButtonEl.addEventListener("click", startQuiz)
 startUp();
