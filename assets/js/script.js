03581bb43e6380533a57ad2186f3844988a344ba
Thomas Ciszek <tciszek515@gmail.com>
2022-03-24 00:00:31 -0400

    added some baseline html, css, and js. Startup page complete.

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var startUpPageEl = document.querySelector("#startup-page");
+var questionsPageEl = document.querySelector("#questions-page");
+var gameOverPageEl = document.querySelector("#game-over-page");
+
+
+
+var startUp = function(){
+questionsPageEl.remove();
+gameOverPageEl.remove();
+}
+
+startUp();
