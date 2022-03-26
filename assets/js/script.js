425068c84f6589d8d4b8554ab8757a8e369e32bd
Thomas Ciszek <tciszek515@gmail.com>
2022-03-26 19:19:49 -0400

    added a timer that starts when the start button is pressed and ends when the timer runs out of time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ var questionsPageEl = document.querySelector("#questions-page");
 var gameOverPageEl = document.querySelector("#game-over-page");
 var startButtonEl = document.querySelector("#start-button");
 var mainParentEl = document.querySelector("#main-parent");
+var timerEl = document.querySelector("#timer");
+var timerGameOverEl = document.querySelector("#timer-end");
+var scoreEl = document.querySelector("#end-score");
 
 var startUp = function(){
   questionsPageEl.remove();
@@ -12,12 +15,29 @@ var startUp = function(){
 var startQuiz = function(){
   startUpPageEl.remove();
   mainParentEl.append(questionsPageEl);
-  /*TimerFunction Start*/
+  countdown();
 }
 
-var gameOver = function(){
+var gameOver = function(timeLeft){
   questionsPageEl.remove();
   mainParentEl.append(gameOverPageEl);
+  timerGameOverEl.textContent = "Time: " + timeLeft;
+  scoreEl.textContent = "Your score is " + timeLeft + " points!"
 }
 startButtonEl.addEventListener("click", startQuiz)
 startUp();
+
+function countdown() {
+  var timeLeft = 45;
+  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function(){
+    if (timeLeft === 0 /*|| questions === done*/){
+      clearInterval(timeInterval);
+      gameOver(timeLeft)
+    }
+    else{
+      timerEl.textContent = "Time: " + timeLeft;
+      timeLeft--;
+    }
+  }, 1000);
+}
